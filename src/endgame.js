5f0b0ba70cb7f42d1b6109936666e01db363f883
Martin Tudor <martcan27@gmail.com>
2018-12-01 15:01:48 +0000

    Update endgame

--- src/endgame.js
+++ src/endgame.js
@@ -1,5 +1,10 @@
 const id01 = document.getElementById("id01");
 const modalScore = document.getElementById("modal-score");
+const modalX = document.getElementById("id01");
+
+modalX.addEventListener("click", function modalGone() {
+    modalX.style.display = "none";
+});
 
 const endGame = function(score) {
     id01.style.display = "block";
