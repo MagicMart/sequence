9b8ae69e71c8da61eed3486d25e30a687e56a31a
Martin Tudor <martcan27@gmail.com>
2019-04-03 20:56:39 +0100

    refactor

--- src/button.js
+++ src/button.js
@@ -3,23 +3,6 @@ import index from "./index.js";
 
 const { handleState, loseALife, oneUp } = index;
 
-const check = function(i) {
-    const input = handleState().userInput;
-    const sequence = handleState().sequence;
-    if (input[i] === sequence[i]) {
-        rightOne();
-        if (input.length === sequence.length) {
-            celebrate();
-            oneUp();
-        } else {
-            return;
-        }
-    } else {
-        wrongOne();
-        loseALife();
-    }
-};
-
 const scorePanel = document.querySelector(".score-panel");
 const buttonsDiv = document.getElementById("buttons");
 const body = document.querySelector("body");
@@ -47,16 +30,28 @@ function celebrate() {
     renderBackground(body, "rgb(153, 255, 102)", 150);
 }
 
-function changeButtonColour(e) {
+function handleButtonClick(e) {
     if (e.target && e.target.classList.contains("button")) {
         const body = document.querySelector("body");
         if (body.style.backgroundColor === "whitesmoke") {
             let orgColor = e.target.classList[1];
             renderBackground(e.target, "rgb(255,255,255)", 150, orgColor);
             handleState({ userInput: [...handleState().userInput, orgColor] });
-            check(handleState().userInput.length - 1);
+            // Check
+            const { userInput, sequence } = handleState();
+            const index = userInput.length - 1;
+            if (userInput[index] === sequence[index]) {
+                rightOne();
+                if (userInput.length === sequence.length) {
+                    celebrate();
+                    oneUp();
+                }
+            } else {
+                wrongOne();
+                loseALife();
+            }
         }
     }
 }
 
-buttonsDiv.addEventListener("click", e => changeButtonColour(e));
+buttonsDiv.addEventListener("click", e => handleButtonClick(e));
